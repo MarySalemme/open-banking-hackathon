c5ee0ce21ff8b4b6c7044d246da286234ddef81b
MarySalemme <marysalemme1@gmail.com>
2018-05-01 17:35:24 +0100

    Add conditional styling for transaction

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './style.css';
 
 function Transaction(props) {
   return (
-      <tr>
+      <tr className={props.amount >= 0 ? style.credit : style.debit}>
         <td>{props.description}</td>
         <td>{props.amount} {props.currency}</td>
       </tr>
