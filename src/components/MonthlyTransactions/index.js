29ecea200d40902b4e36c1794419dab342417ba7
MarySalemme <marysalemme1@gmail.com>
2018-05-01 16:42:38 +0100

    Create monthlyTransactions component

---
+++ src/components/MonthlyTransactions/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Transaction from '../Transaction/index';
+import style from './style.css';
+
+function MonthlyTransactions(props) {
+  return (
+    <div>
+      <h3>{props.month}</h3>
+      <table className={style.transactions}>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+        </tr>
+        {props.transactions.map((transaction) => {
+          return (<Transaction
+            key={transaction.id}
+            description={transaction.description}
+            amount={transaction.amount}
+            currency={transaction.currency}
+          />)
+        })}
+      </table>
+
+    </div>
+  );
+}
+
+export default MonthlyTransactions;
