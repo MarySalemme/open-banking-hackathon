e12c64dc4ec7abadaef3822135c601390275f185
MarySalemme <marysalemme1@gmail.com>
2018-05-02 12:08:21 +0100

    Rename component

---
+++ src/components/TransactionsList/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Transaction from '../Transaction/index';
+import style from './style.css';
+
+function TransactionsList(props) {
+  return (
+    <div className={style.transactionsList}>
+      <h3 className={style.month}>{props.month}</h3>
+      <table className={style.transactions}>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.transactions.map((transaction) => {
+            return (<Transaction
+              key={transaction.id}
+              description={transaction.description}
+              amount={transaction.amount}
+              currency={transaction.currency}
+            />)
+          })}
+        </tbody>  
+      </table>
+
+    </div>
+  );
+}
+
+export default TransactionsList;
