2dcfdd6dba2f430a12b126f8adaf542e8388aaba
MarySalemme <marysalemme1@gmail.com>
2018-05-08 16:36:24 +0100

    Fix styling

--- src/components/TransactionsList/index.js
+++ src/components/TransactionsList/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Transaction from '../Transaction/index';
 import style from './style.css';
 
-const TransactionsList = (props) => {
+const TransactionsList = props => {
   return (
     <div className={style.transactionsList}>
       <h3 className={style.month}>{props.month}</h3>
@@ -14,17 +14,18 @@ const TransactionsList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.transactions.map((transaction) => {
-            return (<Transaction
-              key={transaction.id}
-              description={transaction.description}
-              amount={transaction.amount}
-              currency={transaction.currency}
-            />)
+          {props.transactions.map(transaction => {
+            return (
+              <Transaction
+                key={transaction.id}
+                description={transaction.description}
+                amount={transaction.amount}
+                currency={transaction.currency}
+              />
+            );
           })}
         </tbody>
       </table>
-
     </div>
   );
 };
